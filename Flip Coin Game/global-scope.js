6acde1b4496fca773facbc934ab22347c1075eee
Ergesti <138713806+Ergesti@users.noreply.github.com>
2023-10-05 16:31:29 +0200

    Add files via upload

---
+++ Flip Coin Game/global-scope.js
@@ -0,0 +1,10 @@
+{
+
+    const x = 10
+    let y = 20
+    var z = 30
+
+    console.log(x,  y, z)
+}
+
+console.log(z)
---
+++ Flip Coin Game/underlined-null.js
@@ -0,0 +1,8 @@
+var x
+let y
+
+console.log(x)
+console.log(y)
+console.log("-----------")
+console.log(typeof(x))
+console.log(typeof(y))
